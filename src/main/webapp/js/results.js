f1a7599668668fa8aacd6a5d583195eeb14a9951
RadioactiveMonkey <christophermilner@btintnernet.com>
2018-02-22 13:20:11 +0000

    Added Closing Sources

--- src/main/webapp/js/results.js
+++ src/main/webapp/js/results.js
@@ -1,4 +1,5 @@
-var showingSources = false;
+var OPEN_SOURCE_TEXT = "Expand";
+var CLOSE_SOURCE_TEXT = "Close";
 
 window.onload = function () {
     var sources = document.getElementsByClassName("sources")
@@ -7,20 +8,48 @@ window.onload = function () {
 
         sources[i].onclick = function () {
             var row = this.parentElement;
-            var srcno = row.getAttribute("data-srcno");
 
-            row = row.nextElementSibling;
-            for (var i = 0; i < srcno; i++) {
-                row.classList.remove("hidden");
-
-                var subsrcno = Number(row.getAttribute("data-srcno"));
-                row = row.nextElementSibling;
-                for (var j = 0; j < subsrcno; j++) {
-                    subsrcno += Number(row.getAttribute("data-srcno"));
-                    console.log(subsrcno);
-                    row = row.nextElementSibling;
-                }
-            }
+            if (this.innerHTML === OPEN_SOURCE_TEXT)
+                openSources(row);
+            else
+                closeSources(row);
         }
     }
 };
+
+function openSources(row) {
+    row.getElementsByClassName("sources")[0].innerHTML = CLOSE_SOURCE_TEXT;
+
+    var srcno = row.getAttribute("data-srcno");
+
+    row = row.nextElementSibling;
+    for (var i = 0; i < srcno; i++) {
+        row.classList.remove("hidden");
+
+        var subsrcno = Number(row.getAttribute("data-srcno"));
+        row = row.nextElementSibling;
+        for (var j = 0; j < subsrcno; j++) {
+            subsrcno += Number(row.getAttribute("data-srcno"));
+            row = row.nextElementSibling;
+        }
+    }
+}
+
+function closeSources(row) {
+    row.getElementsByClassName("sources")[0].innerHTML = OPEN_SOURCE_TEXT;
+
+    var srcno = row.getAttribute("data-srcno");
+
+    row = row.nextElementSibling;
+    for (var i = 0; i < srcno; i++) {
+        closeSources(row);
+        row.classList.add("hidden");
+
+        var subsrcno = Number(row.getAttribute("data-srcno"));
+        row = row.nextElementSibling;
+        for (var j = 0; j < subsrcno; j++) {
+            subsrcno += Number(row.getAttribute("data-srcno"));
+            row = row.nextElementSibling;
+        }
+    }
+}
